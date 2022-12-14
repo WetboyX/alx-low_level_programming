c1ac9cfd615c1cb8f1ca224e548f6884f815c404
Osamuyi <isaacosamuyi981@gmail.com>
2022-12-14 03:56:39 -0800

    putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet -> prints the lower alphabets
+ */
+
+void print_alphabet(void)
+{
+	char j;
+
+	for (j = 'a'; j <= 'z'; j++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
+}
